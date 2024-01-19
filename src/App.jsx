c0feea1d39b723b3e853d2232495039368d2e998
Ayush Khatri <khatriayush2003@gmail.com>
2024-01-19 15:35:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div className='flex gap-3 flex-col justify-center text-center items-center h-screen px-10'>
+			<h1 className=' font-bold text-center lg:text-5xl md:text-3xl text-xl '>
+				React + Vite + Tailwind Starter
+			</h1>
+			<p className='text-lg'>
+				This is a starter project for developing websites using React, Vite, and
+				Tailwind CSS.
+			</p>
+		</div>
+	);
+};
+
+export default App;
